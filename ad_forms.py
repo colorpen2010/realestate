56eb3700e328b226d6feeb515f755bd51f316502
GmodHunter <ggmodhunters@gmail.com>
2026-01-12 13:23:58 +0200

    ver 6

--- ad_forms.py
+++ ad_forms.py
@@ -20,25 +20,26 @@ class Ad_form():
         self.ui_adform.cancel.clicked.connect(self.adform.close)
         self.group.addButton(self.ui_adform.rb_residence, 0)
         self.group.addButton(self.ui_adform.rb_commercial, 1)
-        self.tec_row = None
+        self.rid = None
 
     def ad_opener(self):
         self.ui_adform.add_new.setText('add new')
         self.adform.show()
         self.ui_adform.price_line.clear()
         self.ui_adform.address_line.clear()
-        self.tec_row = None
+        self.ui_adform.descruption.clear()
+        self.rid = None
 
-    def ad_editor(self, price, address, type, tec_row):
+    def ad_editor(self, price, address, type, descruption, rid):
         self.ui_adform.add_new.setText('save')
         self.adform.show()
         self.ui_adform.price_line.setText(price)
         self.ui_adform.address_line.setText(address)
+        self.ui_adform.descruption.setText(descruption)
         self.group.button(slovar2[type]).setChecked(True)
-        self.tec_row = tec_row
+        self.rid = rid
 
     def adder(self):
-        self.deff(slovar[self.group.checkedId()], self.ui_adform.price_line.text(), self.ui_adform.address_line.text(),
-                  self.tec_row)
+        self.deff(slovar[self.group.checkedId()], self.ui_adform.price_line.text(), self.ui_adform.address_line.text(),self.ui_adform.descruption.toPlainText(),self.rid)
 
         self.adform.close()
--- admin.py
+++ admin.py
@@ -5,22 +5,39 @@ import ad_forms, os
 from admin_window import Ui_MainWindow
 from form import Ui_Form
 
-def add_ad(type, cost, address, tec_row=None):
+
+def add_ad(type, cost, address,descruption, rid=None):
     global id
-    if tec_row != None:
-        print(tec_row)
-        last_row = tec_row
+    if rid != None:
+        last_row = yoda(rid)
         # id=model.data()
     else:
         last_row = model.rowCount()
         model.insertRow(last_row)
         model.setData(model.index(last_row, 4), id)
         id += 1
-
+    if last_row==-1:
+        print("ia ne mogy rabotat v takix ysloviax")
+        return
     model.setData(model.index(last_row, 0), type)
     model.setData(model.index(last_row, 1), cost)
     model.setData(model.index(last_row, 2), address)
-    kenobi_file_manager={}
+    model.setData(model.index(last_row,3), descruption)
+    saver()
+
+
+def yoda(id):
+    winner=-1
+    for o in range(model.rowCount()):
+        id_candidate = model.data(model.index(o, 4))
+        # print(id_candidate)
+        if id_candidate == id:
+            winner=o
+    return winner
+
+
+def saver():
+    kenobi_file_manager = {}
     ads = []
     for a in range(model.rowCount()):
         slovar3 = {}
@@ -28,11 +45,10 @@ def add_ad(type, cost, address, tec_row=None):
             slovar3[o] = model.data(model.index(a, o))
         ads.append(slovar3)
 
-    kenobi_file_manager['next_ID']=id
-    kenobi_file_manager['ADS']=ads
-    selling = open(os.path.dirname(__file__)+'\\selling.json', "w+")
+    kenobi_file_manager['next_ID'] = id
+    kenobi_file_manager['ADS'] = ads
+    selling = open(os.path.dirname(__file__) + '\\selling.json', "w+")
     json.dump(kenobi_file_manager, selling, indent=4)
-
 
 
 def edit_ad():
@@ -42,8 +58,9 @@ def edit_ad():
     type = model.data(model.index(tec_row, 0))
     cost = model.data(model.index(tec_row, 1))
     address = model.data(model.index(tec_row, 2))
-    rid=model.data(model.index(tec_row,4))
-    ad_form.ad_editor(cost, address, type, rid)
+    desc= model.data(model.index(tec_row,3))
+    rid = model.data(model.index(tec_row, 4))
+    ad_form.ad_editor(cost, address, type,desc, rid)
 
 
 # model=QStandardItemModel(0, 3)
@@ -62,11 +79,11 @@ ui_window.tableWidget.hideColumn(3)
 # ui_window.tableWidget.hideColumn(4)
 
 model = ui_window.tableWidget.model()
-selling = open(os.path.dirname(__file__)+'\\selling.json', "r+")
+selling = open(os.path.dirname(__file__) + '\\selling.json', "r+")
 # print(json.load(selling))
 timed_spisok = json.load(selling)
-id=timed_spisok['next_ID']
-timed_spisok=timed_spisok['ADS']
+id = timed_spisok['next_ID']
+timed_spisok = timed_spisok['ADS']
 selling.close()
 # print(timed_slovar)
 for i in timed_spisok:
